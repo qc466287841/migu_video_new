416e02b244fdb027909473ceb80b95214b0abbb5
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2025-12-13 15:26:46 +0800

    Update fetchURLByAndroid720p.js

--- fetchURLByAndroid720p.js
+++ fetchURLByAndroid720p.js
@@ -14,6 +14,7 @@ async function fetchURLByAndroid720p() {
   printGreen("数据获取成功！")
   // 必须绝对路径
   const path = process.cwd() + '/interface.txt.bak'
+  path = 'interface.txt.bak'
   printBlue(path)
   // 创建写入空内容
   writeFile(path, "")
